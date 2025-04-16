6c088752c9627b39d672289f629896f689bae09e
podobagabor <79839716+podobagabor@users.noreply.github.com>
2025-04-17 01:29:46 +0200

    product fixes

--- src/main/java/hu/bme/ecommercebackend/repository/ProductRepository.java
+++ src/main/java/hu/bme/ecommercebackend/repository/ProductRepository.java
@@ -2,11 +2,17 @@ package hu.bme.ecommercebackend.repository;
 
 import hu.bme.ecommercebackend.model.Category;
 import hu.bme.ecommercebackend.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product,Long>, JpaSpecificationExecutor<Product> {
     boolean existsByCategoryId(Long categoryId);
+    @Query(value = "SELECT DISTINCT p FROM Product p LEFT JOIN FETCH p.category",
+            countQuery = "SELECT COUNT(DISTINCT p) FROM Product p")
+    Page<Product> findAllWithCategory(Pageable pageable);
 }
