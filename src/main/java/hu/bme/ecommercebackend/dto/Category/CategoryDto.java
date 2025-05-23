59756c11ba314a7b8f6c9f3836168ecd144ae060
podobagabor <79839716+podobagabor@users.noreply.github.com>
2025-05-23 23:05:17 +0200

    add lombok equals

--- src/main/java/hu/bme/ecommercebackend/dto/Category/CategoryDto.java
+++ src/main/java/hu/bme/ecommercebackend/dto/Category/CategoryDto.java
@@ -3,17 +3,18 @@ package hu.bme.ecommercebackend.dto.Category;
 import hu.bme.ecommercebackend.model.Category;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@EqualsAndHashCode
 public class CategoryDto {
 
     @NotNull
@@ -36,16 +37,5 @@ public class CategoryDto {
             this.parentCategoryId = null;
         }
         this.subCategoryIds = category.getSubCategories().stream().map(Category::getId).collect(Collectors.toList());
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || obj.getClass() != this.getClass()) {
-            return false;
-        }
-        return (Objects.equals(this.id, ((CategoryDto) obj).getId()) && Objects.equals(this.name, ((CategoryDto) obj).getName()) && Objects.equals(this.subCategoryIds, ((CategoryDto) obj).getSubCategoryIds()) && Objects.equals(this.parentCategoryId, ((CategoryDto) obj).getParentCategoryId()));
     }
 }
--- src/main/java/hu/bme/ecommercebackend/dto/User/CartElementDto.java
+++ src/main/java/hu/bme/ecommercebackend/dto/User/CartElementDto.java
@@ -3,13 +3,14 @@ package hu.bme.ecommercebackend.dto.User;
 import hu.bme.ecommercebackend.dto.Product.ProductDto;
 import hu.bme.ecommercebackend.model.CartElement;
 import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Objects;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class CartElementDto {
     @NotNull
     private Long id;
@@ -24,18 +25,5 @@ public class CartElementDto {
         this.id = cartElement.getId();
         this.productDto = new ProductDto(cartElement.getProduct());
         this.quantity = cartElement.getQuantity();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CartElementDto that = (CartElementDto) o;
-        return Objects.equals(productDto, that.productDto) && Objects.equals(quantity, that.quantity);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(productDto, quantity);
     }
 }
